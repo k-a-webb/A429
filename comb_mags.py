eb6a1aff4e11f7d370898dfea264326fd75fead8
kawebb <kristiannewebb@gmail.com>
2016-01-29 13:37:57 -0800

    used to plot cmd, but replaced by match_phot

--- comb_mags.py
+++ comb_mags.py
@@ -3,44 +3,76 @@ __author__ = 'kawebb'
 import numpy as np
 import argparse
 import pandas as pd
+import os
 
 import matplotlib.pyplot as plt
 from scipy.spatial import KDTree
+import zeropoint
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='For photometry output from sextractor for 3 bands, compile magnitude arrays into singe file')
-    parser.add_argument("--sexfile_j", '-sfj',
+    parser.add_argument("--sexfiles", '-sfs',
+                        nargs='*',
                         action='store',
-                        default='CB68_J_sex_t3_r28.txt',
-                        help='Object.')
-    parser.add_argument("--sexfile_ks", '-sfks',
-                        action='store',
-                        default='CB68_Ks_sex_t3_r28.txt',
-                        help='Object.')
-    parser.add_argument("--sexfile_h", '-sfh',
-                        action='store',
-                        default='CB68_H_sex_t3_r28.txt',
-                        help='Object.')
-    parser.add_argument("--output", '-out',
-                        action='store',
-                        default='CB68_mags.txt',
-                        help='Object.')
+                        default=None,
+                        help='Source extractor files for J,H,Ks bands.')
     parser.add_argument("--maxsep", '-ms',
                         action='store',
                         default=0.0001,
-                        help='Object.')
+                        help='Maximum angular separation for nearest neighbour search.')
+    parser.add_argument("--outfile", '-ofile',
+                        action='store',
+                        default=None,
+                        help='Output file.')
+    parser.add_argument("--outfig", '-ofig',
+                        action='store',
+                        default=None,
+                        help='Output figure.')
+    parser.add_argument("--images", '-is',
+                        nargs='*',
+                        action='store',
+                        default=None,
+                        help='Fits images for J,H,Ks bands.')
 
     args = parser.parse_args()
 
-    phot_j = read_sex(args.sexfile_j)
-    phot_ks = read_sex(args.sexfile_ks)
-    phot_h = read_sex(args.sexfile_h)
+    if args.sexfiles is None:
+        raise Exception, 'No source extractor files given'
+    if args.images is None:
+        raise Exception, 'No image files given'
+    if args.outfile is None:
+        print 'Warning: No output file specified'
+    if args.outfig is None:
+        print 'Warning: No output figure specified'
 
-    sort_by_coords_tree(phot_j, phot_ks, phot_h)
-    mags = sort_by_coords_tree(phot_j, phot_ks, phot_h, args.maxsep)
-    np.savetxt(str(args.output), mags)
+    sexfile_j, sexfile_h, sexfile_ks = args.sexfiles
+    fits_j, fits_h, fits_ks = args.images
+
+    if not os.path.exists(str(args.outfile)):
+
+
+        # remove stars saturated in the fits images, marked with 0 center
+        phot_j = zeropoint.remove_satur(fits_j, sexfile_j)
+        phot_ks = zeropoint.remove_satur(fits_ks, sexfile_ks)
+        phot_h = zeropoint.remove_satur(fits_h, sexfile_h)
+
+        # match the stars in each source extractor file by nearest neighbour
+        mags = sort_by_coords_tree(phot_j, phot_ks, phot_h, args.maxsep)
+
+        # remove stars saturated in any of the source extractor files, marked with mag 99.0
+        mags = mags[mags.mag_j != 99.]
+        mags = mags[mags.mag_ks != 99.]
+        mags = mags[mags.mag_h != 99.]
+
+        mags.to_csv(args.outfile, index=False, sep=' ')
+    else:
+        mags = pd.read_csv(args.outfile, skiprows=1, sep=r"\s*", engine='python',
+                           names=['mag_h', 'mag_j', 'mag_ks', 'magerr_h', 'magerr_j', 'magerr_ks'])
+
+    # (J-H) vs Ks, and (H-Ks) vs Ks
+    plot_cmd(mags.mag_j.values, mags.mag_h.values, mags.mag_ks.values, args.outfig)
 
 
 def sort_by_coords_tree(table_j, table_ks, table_h, max_sep=0.0001):
@@ -48,13 +80,13 @@ def sort_by_coords_tree(table_j, table_ks, table_h, max_sep=0.0001):
     For every star found by the photometry in the J band, look for the same star in the Ks, H bands
     """
 
-    idxs, dups = query_separation_tree(table_j, table_ks, table_h, 0.0001)
+    idxs, dups = query_separation_tree(table_j, table_ks, table_h, max_sep)
 
-    mag_j = table_j.mag_aper.values[idxs[0]]
-    mag_ks = table_ks.mag_aper.values[idxs[1]]
-    mag_h = table_h.mag_aper.values[idxs[2]]
+    mags = {'mag_j': table_j.mag_aper.values[idxs[0]], 'magerr_j': table_j.magerr_aper.values[idxs[0]],
+            'mag_ks': table_ks.mag_aper.values[idxs[1]], 'magerr_ks': table_ks.magerr_aper.values[idxs[1]],
+            'mag_h': table_h.mag_aper.values[idxs[2]], 'magerr_h': table_h.magerr_aper.values[idxs[2]]}
 
-    return np.array((mag_j, mag_ks, mag_h))
+    return pd.DataFrame(data=mags)
 
 
 def query_separation_tree(table1, table2, table3, max_sep=0.0001):
@@ -71,7 +103,7 @@ def query_separation_tree(table1, table2, table3, max_sep=0.0001):
     dups2 = []
     dups3 = []
 
-    for i in table1.index:
+    for i in range(len(table1.index)):
         d2, icoords2 = tree2.query((table1.x_wcs.values[i], table1.y_wcs.values[i]), distance_upper_bound=max_sep)
         d3, icoords3 = tree3.query((table1.x_wcs.values[i], table1.y_wcs.values[i]), distance_upper_bound=max_sep)
         if (d2 != np.inf) & (d3 != np.inf):
@@ -90,63 +122,25 @@ def query_separation_tree(table1, table2, table3, max_sep=0.0001):
     return np.array((idxs1, idxs2, idxs3)), np.array((dups2, dups3))
 
 
-def sort_by_coords(table_j, table_ks, table_h, sep=0.005):
+def plot_cmd(mag_j, mag_h, mag_ks, outfig):
     """
-    For every star found by the photometry in the J band, look for the same star in the Ks, H bands
+    Plot colour magnitude diagrams for (J-H) v. Ks, (H-Ks) v. Ks
     """
 
-    # make lists of indices for stars already matched, make sure not match a star twice
-    imatch_ks = []
-    imatch_h = []
+    fig, ax = plt.subplots(nrows=1, ncols=2)
 
-    mag_table = np.zeros((len(table_j), 3))
-    for i in table_j.index:
-        ra = table_j['x_wcs'][i]
-        dec = table_j['y_wcs'][i]
+    ax[0].scatter(mag_j - mag_h, mag_ks, marker='.')
+    ax[0].set_ylabel('Ks')
+    ax[0].set_xlabel('J-H')
 
-        i_ks, sep_ks = query_separation(table_ks, ra, dec, sep)
-        i_h, sep_h = query_separation(table_h, ra, dec, sep)
+    ax[1].scatter(mag_h - mag_ks, mag_ks, marker='.')
+    ax[1].set_ylabel('Ks')
+    ax[1].set_xlabel('H-Ks')
 
-        if i_ks in imatch_ks:
-            print '  star in Ks band matched twice, {}'.format(i_ks)
-        if i_h in imatch_h:
-            print '  star in H band matched twice, {}'.format(i_h)
+    if outfig is not None:
+        plt.savefig(outfig)
 
-        mag_table[i, :] = table_j['mag_aper'][i], table_ks['mag_aper'][i_ks], table_h['mag_aper'][i_h]
-
-        imatch_ks.append(i_ks)
-        imatch_h.append(i_h)
-
-    return mag_table
-
-
-def query_separation(table, ra, dec, max_sep=0.01):
-    sep = np.sqrt((ra - table.x_wcs.values) ** 2 + (dec - table.y_wcs.values) ** 2)
-    if np.min(sep) > max_sep:
-        print 'separation greater than expected for coordinates {},{}'.format(ra, dec)
-    return np.argmin(sep), np.min(sep)
-
-
-def read_sex(sfile, skiplines=13):
-    #   1 FLUX_APER              Flux vector within fixed circular aperture(s)              [count]
-    #   2 FLUXERR_APER           RMS error vector for aperture flux(es)                     [count]
-    #   3 MAG_APER               Fixed aperture magnitude vector                            [mag]
-    #   4 MAGERR_APER            RMS error vector for fixed aperture mag.                   [mag]
-    #   5 THRESHOLD              Detection threshold above background                       [count]
-    #   6 X_IMAGE                Object position along x                                    [pixel]
-    #   7 Y_IMAGE                Object position along y                                    [pixel]
-    #   8 X_WORLD                Barycenter position along world x axis                     [deg]
-    #   9 Y_WORLD                Barycenter position along world y axis                     [deg]
-    #  10 B_IMAGE                Profile RMS along minor axis                               [pixel]
-    #  11 FWHM_IMAGE             FWHM assuming a gaussian core                              [pixel]
-    #  12 FWHM_WORLD             FWHM assuming a gaussian core                              [deg]
-    #  13 FLAGS                  Extraction flags
-
-    table = pd.read_csv(sfile, skiprows=skiplines, sep=r"\s*", engine='python',
-                        names=['flux_aper', 'fluxerr_aper', 'mag_aper', 'magerr_aper', 'thresh',
-                               'x', 'y', 'x_wcs', 'y_wcs', 'b', 'fhm_image', 'fwhm_world', 'flag'])
-
-    return table
+    plt.show()
 
 
 if __name__ == '__main__':
