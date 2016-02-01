9b4e01381002d134b80fe96330bf44a0f288f1fd
kawebb <kristiannewebb@gmail.com>
2016-01-31 16:05:44 -0800

    reverted to index matching, works well

--- plot_cmd.py
+++ plot_cmd.py
@@ -59,18 +59,18 @@ def main():
     sexfile_j, sexfile_h, sexfile_ks = args.sexfiles
     fits_j, fits_h, fits_ks = args.images
 
-    if not os.path.exists(str(args.outfile)):
+    # if not os.path.exists(str(args.outfile)):
 
-        # remove stars saturated in the fits images, marked with 0 center
-        phot_j = loop_remove_satur(fits_j, sexfile_j, band='J')
-        phot_ks = loop_remove_satur(fits_ks, sexfile_ks, band='Ks')
-        phot_h = loop_remove_satur(fits_h, sexfile_h, band='H')
+    # remove stars saturated in the fits images, marked with 0 center
+    phot_j = loop_remove_satur(fits_j, sexfile_j, band='J')
+    phot_ks = loop_remove_satur(fits_ks, sexfile_ks, band='Ks')
+    phot_h = loop_remove_satur(fits_h, sexfile_h, band='H')
 
-        # match the stars in each source extractor file by nearest neighbour
-        mags = sort_by_coords_tree(phot_j, phot_ks, phot_h, float(args.maxsep))
-        mags.to_csv(args.outfile, index=False)
-    else:
-        mags = pd.read_csv(args.outfile)
+    # match the stars in each source extractor file by nearest neighbour
+    mags = sort_by_coords_tree(phot_j, phot_ks, phot_h, float(args.maxsep))
+    # mags.to_csv(args.outfile, index=False)
+    # else:
+    #     mags = pd.read_csv(args.outfile)
 
     if args.toplot:
         # plot to make sure matching the same points
@@ -89,31 +89,43 @@ def sort_by_coords_tree(table_j, table_ks, table_h, max_sep=0.0001):
     For every star found by the photometry in the J band, look for the same star in the Ks, H bands
     """
 
-    # idxs = query_tree3(table_j, table_ks, table_h, max_sep)
+    idxs = query_tree3(table_j, table_ks, table_h, max_sep)
+
+    mags = {'mag_aper_J': table_j['mag_aper_J'].values[idxs[0]], 'magerr_aper_J': table_j['magerr_aper_J'].values[idxs[0]],
+            'mag_aper_Ks': table_ks['mag_aper_Ks'].values[idxs[1]], 'magerr_aper_Ks': table_ks['magerr_aper_Ks'].values[idxs[1]],
+            'mag_aper_H': table_h['mag_aper_H'].values[idxs[2]], 'magerr_aper_H': table_h['magerr_aper_H'].values[idxs[2]]}
+
+    return pd.DataFrame(data=mags)
+
+    # idxsj, idxsks, idxsh = query_tree3(table_j, table_ks, table_h, max_sep)
     #
-    # mags = {'mag_j': table_j.mag_aper.values[idxs[0]], 'magerr_j': table_j.magerr_aper.values[idxs[0]],
-    #         'mag_ks': table_ks.mag_aper.values[idxs[1]], 'magerr_ks': table_ks.magerr_aper.values[idxs[1]],
-    #         'mag_h': table_h.mag_aper.values[idxs[2]], 'magerr_h': table_h.magerr_aper.values[idxs[2]]}
+    # spliced_table_j = table_j[idxsj]  # apply True/False array to splice out matched indexes
+    # spliced_table_j.reset_index(inplace=True, drop=True)
+    # spliced_table_ks = table_ks[idxsks]
+    # spliced_table_ks.reset_index(inplace=True, drop=True)
+    # spliced_table_h = table_h[idxsh]
+    # spliced_table_h.reset_index(inplace=True, drop=True)
     #
-    # return pd.DataFrame(data=mags)
-
-    idxsj, idxsks, idxsh = query_tree3(table_j, table_ks, table_h, max_sep)
-
-    spliced_table_j = table_j[idxsj]  # apply True/False array to splice out matched indexes
-    spliced_table_j.reset_index(inplace=True, drop=True)
-    spliced_table_ks = table_ks[idxsks]
-    spliced_table_ks.reset_index(inplace=True, drop=True)
-    spliced_table_h = table_h[idxsh]
-    spliced_table_h.reset_index(inplace=True, drop=True)
-
-    sorted_table_j = spliced_table_j.sort_values('x_wcs_J')  # sort both tables by the x axis
-    sorted_table_ks = spliced_table_ks.sort_values('x_wcs_Ks')
-    sorted_table_h = spliced_table_h.sort_values('x_wcs_H')
-
-    sorted_table_ks.drop(['kron_radius', 'fwhm_image', 'fwhm_world', 'flags'], axis=1, inplace=True)
-    sorted_table_h.drop(['kron_radius', 'fwhm_image', 'fwhm_world', 'flags'], axis=1, inplace=True)
-
-    return pd.concat((sorted_table_j, sorted_table_ks, sorted_table_h), axis=1)
+    # sorted_table_j = spliced_table_j.sort_values('x_wcs_J')  # sort both tables by the x axis
+    # sorted_table_ks = spliced_table_ks.sort_values('x_wcs_Ks')
+    # sorted_table_h = spliced_table_h.sort_values('x_wcs_H')
+    #
+    # plt.plot(sorted_table_j['x_pix_J'].values, color='k', label='J')
+    # plt.plot(sorted_table_ks['x_pix_Ks'].values, color='r', label='Ks')
+    # plt.plot(sorted_table_h['x_pix_H'].values, color='g', label='H')
+    # plt.legend()
+    # plt.show()
+    #
+    # plt.plot(sorted_table_j['y_pix_J'].values, color='k', label='J')
+    # plt.plot(sorted_table_ks['y_pix_Ks'].values, color='r', label='Ks')
+    # plt.plot(sorted_table_h['y_pix_H'].values, color='g', label='H')
+    # plt.legend()
+    # plt.show()
+    #
+    # sorted_table_ks.drop(['kron_radius', 'fwhm_image', 'fwhm_world', 'flags'], axis=1, inplace=True)
+    # sorted_table_h.drop(['kron_radius', 'fwhm_image', 'fwhm_world', 'flags'], axis=1, inplace=True)
+    #
+    # return pd.concat((sorted_table_j, sorted_table_ks, sorted_table_h), axis=1)
 
 
 def query_tree3(table_j, table_ks, table_h, max_sep=0.0001):
@@ -121,59 +133,59 @@ def query_tree3(table_j, table_ks, table_h, max_sep=0.0001):
     For two pandas database files, with the ra/dec header names x_wcs/y_wcs as defined in read_sex
     """
 
-    # tree2 = KDTree(zip(table2.x_wcs.values.ravel(), table2.y_wcs.values.ravel()))
-    # tree3 = KDTree(zip(table3.x_wcs.values.ravel(), table3.y_wcs.values.ravel()))
-    #
-    # idxs1 = []
-    # idxs2 = []
-    # idxs3 = []
-    # dups2 = []
-    # dups3 = []
-    #
-    # for i in range(len(table1.index)):
-    #     d2, icoords2 = tree2.query((table1.x_wcs.values[i], table1.y_wcs.values[i]), distance_upper_bound=max_sep)
-    #     d3, icoords3 = tree3.query((table1.x_wcs.values[i], table1.y_wcs.values[i]), distance_upper_bound=max_sep)
-    #     if (d2 != np.inf) & (d3 != np.inf):
-    #         if icoords2 in idxs2:
-    #             dups2.append(icoords2)
-    #         if icoords3 in idxs3:
-    #             dups3.append(icoords3)
-    #
-    #         idxs1.append(i)
-    #         idxs2.append(icoords2)
-    #         idxs3.append(icoords3)
-    #
-    # if (len(dups2) > 0) | (len(dups3) > 0):
-    #     raise Exception, 'WARNING: duplicates found'
-    #
-    # return np.array((idxs1, idxs2, idxs3))
+    tree2 = KDTree(zip(table_ks['x_wcs_Ks'].values.ravel(), table_ks['y_wcs_Ks'].values.ravel()))
+    tree3 = KDTree(zip(table_h['x_wcs_H'].values.ravel(), table_h['y_wcs_H'].values.ravel()))
 
-    tree_ks = KDTree(zip(table_ks['x_wcs_Ks'].values.ravel(), table_ks['y_wcs_Ks'].values.ravel()))
-    tree_h = KDTree(zip(table_h['x_wcs_H'].values.ravel(), table_h['y_wcs_H'].values.ravel()))
+    idxs1 = []
+    idxs2 = []
+    idxs3 = []
+    dups2 = []
+    dups3 = []
 
-    idxs_j = np.zeros(len(table_j), dtype=bool)
-    idxs_ks = np.zeros(len(table_ks), dtype=bool)
-    idxs_h = np.zeros(len(table_h), dtype=bool)
-
-    for i in range(len(table_j)):
-        d2, i2 = tree_ks.query((table_j['x_wcs_J'].values[i], table_j['y_wcs_J'].values[i]),
-                               distance_upper_bound=max_sep)
-        d3, i3 = tree_h.query((table_j['x_wcs_J'].values[i], table_j['y_wcs_J'].values[i]),
-                              distance_upper_bound=max_sep)
+    for i in range(len(table_j.index)):
+        d2, icoords2 = tree2.query((table_j['x_wcs_J'].values[i], table_j['y_wcs_J'].values[i]), distance_upper_bound=max_sep)
+        d3, icoords3 = tree3.query((table_j['x_wcs_J'].values[i], table_j['y_wcs_J'].values[i]), distance_upper_bound=max_sep)
         if (d2 != np.inf) & (d3 != np.inf):
-            if (idxs_j[i] is True) | (idxs_ks[i2] is True) | (idxs_h[i3] is True):
-                print '  WARNING: duplicates found for index {}'.format(i)
-            idxs_j[i] = True
-            idxs_ks[i2] = True
-            idxs_h[i3] = True
-        else:
-            print 'no match index {}'.format(i)
+            if icoords2 in idxs2:
+                dups2.append(icoords2)
+            if icoords3 in idxs3:
+                dups3.append(icoords3)
 
-    if (np.count_nonzero(idxs_j) != np.count_nonzero(idxs_ks)) | (np.count_nonzero(idxs_j) != np.count_nonzero(idxs_h)):
-        print 'Number of indexes dont match: {}, {}, {}'.format(np.count_nonzero(idxs_j), np.count_nonzero(idxs_ks),
-                                                                np.count_nonzero(idxs_h))
+            idxs1.append(i)
+            idxs2.append(icoords2)
+            idxs3.append(icoords3)
 
-    return idxs_j, idxs_ks, idxs_h
+    if (len(dups2) > 0) | (len(dups3) > 0):
+        raise Exception, 'WARNING: duplicates found'
+
+    return np.array((idxs1, idxs2, idxs3))
+
+    # tree_ks = KDTree(zip(table_ks['x_wcs_Ks'].values.ravel(), table_ks['y_wcs_Ks'].values.ravel()))
+    # tree_h = KDTree(zip(table_h['x_wcs_H'].values.ravel(), table_h['y_wcs_H'].values.ravel()))
+    #
+    # idxs_j = np.zeros(len(table_j), dtype=bool)
+    # idxs_ks = np.zeros(len(table_ks), dtype=bool)
+    # idxs_h = np.zeros(len(table_h), dtype=bool)
+    #
+    # for i in range(len(table_j)):
+    #     d2, i2 = tree_ks.query((table_j['x_wcs_J'].values[i], table_j['y_wcs_J'].values[i]),
+    #                            distance_upper_bound=max_sep)
+    #     d3, i3 = tree_h.query((table_j['x_wcs_J'].values[i], table_j['y_wcs_J'].values[i]),
+    #                           distance_upper_bound=max_sep)
+    #     if (d2 != np.inf) & (d3 != np.inf):
+    #         if (idxs_j[i] is True) | (idxs_ks[i2] is True) | (idxs_h[i3] is True):
+    #             print '  WARNING: duplicates found for index {}'.format(i)
+    #         idxs_j[i] = True
+    #         idxs_ks[i2] = True
+    #         idxs_h[i3] = True
+    #     else:
+    #         print 'no match index {}'.format(i)
+    #
+    # if (np.count_nonzero(idxs_j) != np.count_nonzero(idxs_ks)) | (np.count_nonzero(idxs_j) != np.count_nonzero(idxs_h)):
+    #     print 'Number of indexes dont match: {}, {}, {}'.format(np.count_nonzero(idxs_j), np.count_nonzero(idxs_ks),
+    #                                                             np.count_nonzero(idxs_h))
+    #
+    # return idxs_j, idxs_ks, idxs_h
 
 
 def loop_remove_satur(image, sexfile, band=None):
