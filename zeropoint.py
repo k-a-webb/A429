4e2220c66716b3e134aaeb0cfb3b4d5e1a560cc9
kawebb <kristiannewebb@gmail.com>
2016-02-21 21:02:53 -0800

    updated to include proper band calcualtion

--- zeropoint.py
+++ zeropoint.py
@@ -6,9 +6,9 @@ Determine the magnitude zeropoint of our CFHT observations by comparing bright
 unsaturated stars to the same stars in the 2mass catalog.
 Apply this zeropoint to all of the stars measured.
 
-python zeropoint.py -img CB68/CB68_J_sub.fits -ph CB68/CB68_J_mags_t20.txt -ap 30 -tm CB68/2mass_CB68.tbl -out CB68/CB68_offsets.txt
-python zeropoint.py -img CB68/CB68_H_sub.fits -ph CB68/CB68_H_mags_t20.txt -ap 30 -tm CB68/2mass_CB68.tbl -out CB68/CB68_offsets.txt
-python zeropoint.py -img CB68/CB68_Ks_sub.fits -ph CB68/CB68_Ks_mags_t20.txt -ap 24 -tm CB68/2mass_CB68.tbl -out CB68/CB68_offsets.txt
+python zeropoint.py -img CB68/CB68_J_sub.fits -ph phot_t20/CB68_J_mags_t20.txt -band j -ap 30 -tm CB68/2mass_CB68.tbl -out CB68/CB68_offsets.txt  -sf phot_t20/CB68_J_sex_t20.txt
+python zeropoint.py -img CB68/CB68_H_sub.fits -ph phot_t20/CB68_H_mags_t20.txt -band h -ap 30 -tm CB68/2mass_CB68.tbl -out CB68/CB68_offsets.txt -sf phot_t20/CB68_H_sex_t20.txt
+python zeropoint.py -img CB68/CB68_Ks_sub.fits -ph phot_t20/CB68_Ks_mags_t20.txt -band k -ap 24 -tm CB68/2mass_CB68.tbl -out CB68/CB68_offsets.txt -sf phot_t20/CB68_Ks_sex_t20.txt
 
 sex -c phot_t3.sex ../release/CB68_J_sub.fits -CATALOG_NAME CB68_J_sex_t3_ap30.txt -PHOT_APERTURES 30 -MAG_ZEROPOINT 30-0.192943311392
 sex -c phot_t3.sex ../release/CB68_Ks_sub.fits -CATALOG_NAME CB68_Ks_sex_t3_ap24.txt -PHOT_APERTURES 24 -MAG_ZEROPOINT 30+0.605746118244
@@ -19,13 +19,8 @@ sex -c phot_t3.sex ../release/CB68_H_sub.fits -CATALOG_NAME CB68_H_sex_t3_ap40.t
 import os
 import numpy as np
 import argparse
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
-from astropy.io import fits
+from astropy.io import ascii
 import phot_curves
-
-TEMP_FILE = '/Users/kawebb/a429/temp.txt'
 
 
 def main():
@@ -55,6 +50,10 @@ def main():
                         action='store',
                         default=None,
                         help='File of photometry')
+    parser.add_argument("--band", '-band',
+                        action='store',
+                        default=None,
+                        help='Band of photometry')
     args = parser.parse_args()
 
     if args.image is None:
@@ -67,138 +66,116 @@ def main():
         raise Exception, 'ERROR: No souce extractor file or photometry file given'
     if args.tmass is None:
         raise Exception, 'ERROR: No 2Mass file given'
+    if args.band is None:
+        raise Exception, 'ERROR: No band given'
 
+    apertures = [5, 10, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60]
+    iap = np.argmin(np.array(apertures) - int(args.aperture)) + 1
+
+    zeropoint(args.image, args.sexfile, args.outfile, args.photfile, args.tmass, iap, args.band)
+
+
+def zeropoint(image, sexfile, outfile, photfile, twomassfile, iap, band):
     clouds = ['CB68', 'L429', 'L1521E', 'L1544', 'L1552']
     centers = np.array([[2610.6899, 2868.1778], [2496.3232, 2158.1909], [2532.6025, 2753.7333], [2345.069, 2855.932],
                         [2710.337, 2593.2019]])  # in pixels
     sizes = np.array([[1382.2207, 1227.3661], [1869.7259, 2345.7605], [1880.5105, 1777.3177], [1788.7782, 1570.9142],
                       [1639.7134, 177.3117]])  # in pixels
     for i, cloud in enumerate(clouds):
-        if cloud in args.image:
+        if cloud in image:
             center = centers[i]
             size = sizes[i]
 
-    if args.photfile is not None:
-        unsat_list = pd.read_csv(args.photfile)
+    # read in catalogue of unsaturated stars
+    if not os.path.exists(photfile):
+        photfile = phot_curves.remove_saturated(image, sexfile, photfile)
     else:
-        imgdata, imghead = phot_curves.read_fits(args.image)
-        phottable = phot_curves.read_sex(args.sexfile)
-        unsat_list = remove_satur(imgdata, phottable, args.toplot)  # create list of unsatured stars identified by sex
+        photfile = ascii.read(photfile)
 
-    unreddened_list = remove_region(unsat_list, center, size)  # Remove stars reddened from zeropoint calculation
+    # Removed reddened stars from zeropoint calculation
+    idx_reddened = id_region(photfile['X_IMAGE'], photfile['Y_IMAGE'], center, size)  # Find reddened
+    unreddened = photfile[~idx_reddened]
 
-    starpsf_idx = phot_curves.half_light(unreddened_list['mag_aper_{}'.format(args.aperture)].values,
-                                         unreddened_list['flux_radius'].values)
-    starpsf_list = unreddened_list[starpsf_idx]
-    starpsf_list.reset_index()
+    # Remove objects which are not stars by PSF
+    idx_stars = phot_curves.half_light(unreddened['MAG_APER_{}'.format(iap)], unreddened['FLUX_RADIUS'], toplot=False)
+    stars = unreddened[idx_stars]
 
-    twomass_list, star_list = parse_2mass(args.tmass, starpsf_list)
-    offset = compare_magnitudes(star_list, twomass_list, args.aperture, args.outfile)
+    # Compare magnitudes with same in 2Mass catalogue
+    twomass, stars_tm = parse_2mass(twomassfile, stars)
+    offset = compare_magnitudes(stars_tm['MAG_APER_{}'.format(iap)], twomass['{}_m'.format(band)],
+                                stars_tm['MAGERR_APER_{}'.format(iap)], outfile)
 
 
-def compare_magnitudes(uslist, tmstars, aperture=None, outfile=None):
+def id_region(x, y, center, size):
     """
-    Comparing magnitudes from source extractor and 2mass.
-    Calculate the magnitude offset between the two to find correction to zeropoint in source extractor
+    For a given centerpoint and size of a rectangular region (in pixels), remove stars in the region from the list
     """
 
-    tmmag = tmstars.j_m.values
-    sexmag = uslist['mag_aper_{}'.format(aperture)].values
-    sexmagerr = uslist['magerr_aper_{}'.format(aperture)].values
+    y_min = center[1] - size[1] / 2.
+    y_max = center[1] + size[1] / 2.
+    x_min = center[0] - size[0] / 2.
+    x_max = center[0] + size[0] / 2.
 
-    wgt = (sexmagerr) ** (-2)
+    # idxx = np.array(x[np.where(x > x_min)] < x_max)
+    # idxy = np.array(y[np.where(y > y_min)] < y_max)
+    # idx = np.intersect1d(np.arange(len(x))[idxx], np.arange(len(y))[idxy])
 
-    offset = tmmag - sexmag
-    wavg_off = np.average(offset, weights=wgt)
-    var_off = np.var(offset)
-    sig_off = np.sqrt(np.sum(wgt))
-
-    wavg_mag = np.average(sexmag, weights=wgt)
-    var_mag = np.var(sexmag)
-
-    print '{} {} {} {} {} {}'.format(aperture, wavg_off, var_off, sig_off, wavg_mag, var_mag)
-    if outfile is not None:
-        with open(outfile, 'a') as of:
-            of.write(
-                '& {} & {} & {} & {} & {} & {} \\\\ \n'.format(aperture, wavg_off, var_off, sig_off, wavg_mag, var_mag))
-
-    return wavg_off
+    idxx = np.arange(len(x))[(x_min < x) * (x < x_max)]
+    idxy = np.arange(len(y))[(y_min < y) * (y < y_max)]
+    idx = np.intersect1d(idxx, idxy)
+    return idx
 
 
-def parse_2mass(twomass_file, star_list):
+def parse_2mass(twomass_file, stars):
     """
     For a given list of coordinates returned by source extractor, find the same star in the
     2Mass catalog, and save the cropped catalog list to a file
     """
 
-    print '  Identifying 2Mass catalog stars with Unsaturated stars in CFHT image'
+    print '  Identifying 2Mass catalog stars with stars in CFHT image'
 
-    twomass_list = read_2mass(twomass_file)
+    twomass = ascii.read(twomass_file)
 
     idx_stars = []
     idx_2mass = []
-    for ii, i in enumerate(star_list.index):
+    for i in range(len(stars)):
         max_separation = 0.005
-        imin, sep_min = query_separation(twomass_list, star_list.x_wcs[i], star_list.y_wcs[i])
+        imin, sep_min = query_separation((twomass['ra'], twomass['dec']), (stars['X_WORLD'][i], stars['Y_WORLD'][i]))
         if sep_min < max_separation:
             idx_stars.append(i)
             idx_2mass.append(imin)
-            # else:
-            #     print '  {},{}  {},{} {}'.format(star_list.x_wcs[i], star_list.y_wcs[i], twomass_list['ra'][i_min],
-            #                                      twomass_list['dec'][i_min], sep_min)
 
-    return twomass_list.loc[idx_2mass, :], star_list.loc[idx_stars, :]
+    return twomass[idx_2mass], stars[idx_stars]
 
 
-def query_separation(table, ra, dec):
-    sep = np.sqrt((ra - table.ra.values) ** 2 + (dec - table.dec.values) ** 2)
+def query_separation((x1, y1), (x2, y2)):
+    sep = np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
     return np.argmin(sep), np.min(sep)
 
 
-def remove_region(starlist, center, size):
+def compare_magnitudes(mag_sex, mag_tm, magerr, outfile=None):
     """
-    For a given centerpoint and size of a rectangular region (in pixels), remove stars in the region from the list
-    """
-
-    in_x = starlist.query('{} < x_pix < {}'.format(center[0] - size[0] / 2, center[0] + size[0] / 2))
-    in_xy = in_x.query('{} < y_pix < {}'.format(center[1] - size[1] / 2, center[1] + size[1] / 2))
-
-    starlist = starlist.drop(starlist.index[in_xy.index.values])
-    starlist.reset_index(inplace=True, drop=True)  # drop index column (inherent to pandas dataframe) as out of order
-
-    return starlist
-
-
-def remove_satur(fdata, stable, toplot=False):
-    """
-    Detect saturated stars in the image, and remove them from the list returned by source extractor
-    In our images, saturated stars have low (0) values at the center rather than a very high value
-    and are therefore not identified by source extractor automatticaly.
+    Comparing magnitudes from source extractor and 2mass.
+    Calculate the magnitude offset between the two to find correction to zeropoint in source extractor
     """
 
-    # Iterate through stars
-    ix = phot_curves.detect_satur(fdata, stable['x_pix'].values, stable['y_pix'].values, stable['kron_radius'].values)
-    unsat_stable = stable[ix]
+    wgt = (magerr) ** (-2)
 
-    if toplot:
-        plt.imshow(fdata, origin='lower', norm=matplotlib.colors.LogNorm())
-        sat_stable = unsat_stable[np.invert(ix)]
-        plt.plot(sat_stable.x.values, sat_stable.y.values, 'x')
-        plt.title('Saturated stars')
-        plt.show()
+    offset = mag_tm - mag_sex
+    wavg_off = np.average(offset, weights=wgt)
+    var_off = np.var(offset)
+    sig_off = np.sqrt(np.sum(wgt))
 
-    return unsat_stable
+    wavg_mag = np.average(mag_sex, weights=wgt)
+    var_mag = np.var(mag_sex)
 
+    print '{} {} {} {} {}'.format(wavg_off, var_off, sig_off, wavg_mag, var_mag)
+    if outfile is not None:
+        with open(outfile, 'a') as of:
+            of.write(
+                '& {} & {} & {} & {} & {} \\\\ \n'.format(wavg_off, var_off, sig_off, wavg_mag, var_mag))
 
-def read_2mass(tmfile, skiplines=55):
-    assert os.path.exists(tmfile), 'ERROR: 2Mass file {} does not exist'.format(tmfile)
-    # ra, dec, j_m, j_cmsig, j_msigcom, j_snr, h_m, h_cmsig, h_msigcom, h_snr, k_m, k_cmsig, k_msigcom, k_snr
-    #   rd_flg, dist, angle, j_h, h_k, j_k
-    table = pd.read_csv(tmfile, skiprows=skiplines, sep=r"\s*", engine='python',
-                        names=['ra', 'dec', 'j_m', 'j_cmsig', 'j_msigcom', 'j_snr', 'h_m', 'h_cmsig', 'h_msigcom',
-                               'h_snr', 'k_m', 'k_cmsig', 'k_msigcom', 'k_snr', 'rd_flg', 'dist', 'angle', 'j_h',
-                               'h_k', 'j_k'])
-    return table
+    return wavg_off
 
 
 if __name__ == '__main__':
