c204a436048dc8eaf6650df4281fe9bd3dfb2ab8
kawebb <kristiannewebb@gmail.com>
2016-02-21 17:29:40 -0800

    updated to a working end-to-end copy

--- addstar.py
+++ addstar.py
@@ -2,7 +2,7 @@ import argparse
 from pyraf import iraf
 import os
 import numpy as np
-from astropy.io import ascii
+from astropy.io import ascii, fits
 from scipy.spatial import KDTree
 import matplotlib.pyplot as plt
 
@@ -59,48 +59,10 @@ def main():
                         action='store',
                         default=None,
                         help='File path for output files.')
-    parser.add_argument("--addphot", '-addphot',
+    parser.add_argument("--artphotfile", '-artphot',
                         action='store',
                         default=None,
                         help='Input file of source extractor file with artificial stars added.')
-
-    # optional
-    parser.add_argument("--outcoords", '-coo',
-                        action='store',
-                        default=None,
-                        help='Outfile of coordinates [wcs].')
-    parser.add_argument("--outphot", '-phot',
-                        action='store',
-                        default=None,
-                        help='Outfile of daophot photometry.')
-    parser.add_argument("--outsexphot", '-sexphot',
-                        action='store',
-                        default=None,
-                        help='Outfile of daophot photometry with source extractor photometry.')
-    parser.add_argument("--outpst", '-pst',
-                        action='store',
-                        default=None,
-                        help='Outfile of daophot pst.')
-    parser.add_argument("--outopst", '-opst',
-                        action='store',
-                        default=None,
-                        help='Outfile of daophot pst.')
-    parser.add_argument("--outgroup", '-group',
-                        action='store',
-                        default=None,
-                        help='Outfile of daophot pst groupfile.')
-    parser.add_argument("--outpsfimg", '-psfimg',
-                        action='store',
-                        default=None,
-                        help='Outfile of daophot psf image.')
-    parser.add_argument("--outartphot", '-artphot',
-                        action='store',
-                        default=None,
-                        help='Outfile of artificial star positions and magnitudes.')
-    parser.add_argument("--outaddimage", '-addimg',
-                        action='store',
-                        default=None,
-                        help='Outfile image with artificial stars.')
     args = parser.parse_args()
 
     if args.sexfile is None:
@@ -108,9 +70,7 @@ def main():
     if args.image is None:
         raise Exception, 'No input image file given'
 
-    artphotfile = run_daophot(args.sexfile, args.image, args.outpath, args.aperture, args.zeropoint, args.outcoords,
-                              args.outphot, args.outsexphot, args.outpst, args.outopst, args.outgroup, args.outpsfimg,
-                              args.outartphot, args.outaddimage)
+    artphotfile = run_daophot(args.sexfile, args.image, args.outpath, args.artphotfile, args.aperture, args.zeropoint)
 
     print 'sex -c phot_t3.sex CB68_J_sub.add.fits -CATALOG_NAME CB68_J_sex_t3_ap30_add.txt -PHOT_APERTURES 30 -MAG_ZEROPOINT 29.80705'
 
@@ -132,94 +92,77 @@ def find_art_stars(addphot, artphotfile, max_sep=0.1):
 
     plt.hist(np.array(mag), 100, color='g')
     plt.hist(np.array(mag)[idx_found], 100, color='b', alpha=0.5)
-    plt.xlim(10,30)
+    plt.xlim(10, 30)
     plt.show()
 
 
-def run_daophot(sexfile, image, outpath, aperture=None, zeropoint=None, coords=None, photfile=None, sexphotfile=None,
-                pstfile=None, opstfile=None, groupfile=None, psfimage=None, maxnpsf=25., artphotfile=None,
-                addimage=None):
-    try:
-        prefix_sex = (sexfile.split('/')[-1]).split('.')[0]
-    except IndexError:
-        prefix_sex = sexfile.split('.')[0]
+def run_daophot(sexfile, image, outpath, artphotfile, aperture=None, zeropoint=None, maxnpsf=50., nstars=3000):
     try:
         prefix_img = (image.split('/')[-1]).split('.')[0]
     except IndexError:
         prefix_img = sexfile.split('.')[0]
 
-    if coords is None:
-        coords = os.path.join(outpath, prefix_sex + '.coo')
-    if photfile is None:
-        photfile = os.path.join(outpath, prefix_sex + '.phot')
-    if sexphotfile is None:
-        sexphotfile = os.path.join(outpath, prefix_sex + '.mag')
-    if pstfile is None:
-        pstfile = os.path.join(outpath, prefix_sex + '.pst')
-    if opstfile is None:
-        opstfile = os.path.join(outpath, prefix_sex + '.opst')
-    if groupfile is None:
-        groupfile = os.path.join(outpath, prefix_sex + '.psg')
-    if psfimage is None:
-        psfimage = os.path.join(outpath, prefix_img + '.psf.fits')
-    if artphotfile is None:
-        artphotfile = os.path.join(outpath, prefix_sex + '.art')
-    if addimage is None:
-        addimage = os.path.join(outpath, prefix_img + '.add.fits')
-
-    sex_data = ascii.read(sexfile, format='sextractor')
-    np.savetxt(coords, np.transpose((np.array(sex_data['X_WORLD']), np.array(sex_data['Y_WORLD']))), delimiter=' ')
-
-    bkg = np.std(np.array(sex_data['BACKGROUND']))
-    print bkg
+    coofile = os.path.join(outpath, prefix_img + '.coo')
+    coofile2 = os.path.join(outpath, prefix_img + '.coo.2')
+    photfile = os.path.join(outpath, prefix_img + '.phot')
+    sexphotfile = os.path.join(outpath, prefix_img + '.mag')
+    pstfile = os.path.join(outpath, prefix_img + '.pst')
+    opstfile = os.path.join(outpath, prefix_img + '.opst')
+    groupfile = os.path.join(outpath, prefix_img + '.psg')
+    psfimage = os.path.join(outpath, prefix_img + '.psf')
+    addimage = os.path.join(outpath, prefix_img + '.add')
 
     iraf.daophot()
-    # phot -- do aperture photometry on a list of stars
-    # phot image
-    if not os.path.exists(photfile):
-        if aperture is None:
-            raise Exception, 'No input aperture parameter given'
-        if zeropoint is None:
-            raise Exception, 'No input zeropoint value given'
-        iraf.daophot.phot(image, coords=coords, output=photfile, wcsin="world", apertures=float(aperture),
-                          zmag=float(zeropoint), verbose=True)  # , annulus=aperture + 10, dannulus=10,
-    else:
-        print 'File {} already exists'.format(photfile)
 
+    # daofind - automatically detect objects in an image
+    if not os.path.exists(coofile):
+        iraf.daophot.daofind(image=image, output=coofile)
+    print 'done with {}'.format(coofile)
+
+    # remove saturated stars from coordinate file
+    if not os.path.exists(coofile2):
+        remove_saturated(image, sexfile, coofile, coofile2)
+    print 'done with {}'.format(coofile2)
+
+    # phot -- do aperture photometry on a list of stars
+    if not os.path.exists(photfile):
+        raise Exception, 'STOP HERE AND RUN DAOPHOT PHOT IN IRAF, bkgstd=100, fwhm=3'
+        # if aperture is None:
+        #     raise Exception, 'No input aperture parameter given'
+        # if zeropoint is None:
+        #     raise Exception, 'No input zeropoint value given'
+        # iraf.daophot.phot(image, coords=coofile2, output=photfile, wcsin="world", apertures=float(aperture),
+        #                   zmag=float(zeropoint), verbose=True)
+    print 'done with {}'.format(photfile)
+
+    # replace phot values with those from source extractor
     if not os.path.exists(sexphotfile):
         write_phot(photfile, sexphotfile, sexfile)
+    print 'done with {}'.format(sexphotfile)
 
     # pstselect -- select candidate psf stars from a photometry file
-    # pstselect image photfile pstfile maxnpsf
     if not os.path.exists(pstfile):
         iraf.daophot.pstselect(image, sexphotfile, pstfile, maxnpsf)
-    else:
-        print 'File {} already exists'.format(pstfile)
+    print 'done with {}'.format(pstfile)
 
     # psf -- build the point spread function for an image
-    # psf image photfile pstfile psfimage opstfile groupfile
     if not os.path.exists(psfimage):
-        iraf.daophot.psf(image, photfile=sexphotfile, pstfile=pstfile, psfimage=psfimage, opstfile=opstfile,
-                         groupfile=groupfile, interactive=False)
-    else:
-        print 'File {} already exists'.format(psfimage)
+        raise Exception, 'STOP HERE AND RUN DAOPHOT PSF IN IRAF, max=50000, rad=11'
+        # iraf.daophot.psf(image, photfile=sexphotfile, pstfile=pstfile, psfimage=psfimage, opstfile=opstfile,
+        #                  groupfile=groupfile, interactive=False)
+    print 'done with {}'.format(psfimage)
 
     # define parameters of artificial stars
     if not os.path.exists(artphotfile):
-        nstars = 3000
+        sex_data = ascii.read(sexfile, format='sextractor')
         x = random_sample(nstars, sex_data['X_WORLD'])
         y = random_sample(nstars, sex_data['Y_WORLD'])
         mags = random_sample(nstars, np.linspace(18., 28., nstars))
         np.savetxt(artphotfile, np.transpose([x, y, mags]), delimiter=' ')
-    else:
-        print 'File {} already exists'.format(artphotfile)
 
     # addstar -- add artificial stars to images
-    # addstar image photfile psfimage addimage
     if not os.path.exists(addimage):
         iraf.daophot.addstar(image, artphotfile, psfimage, addimage)  # 50% recovery
-    else:
-        print 'File {} already exists'.format(addimage)
 
     return artphotfile
 
@@ -239,13 +182,17 @@ def write_phot(photfile, newphotfile, sexfile):
 
     tree = KDTree(zip(phot['XCENTER'].ravel(), phot['YCENTER'].ravel()))
     max_sep = 0.1
-    idxs = []
-    for i in range(len(sex)):
-        d, i = tree.query((sex['X_IMAGE'][i], sex['Y_IMAGE'][i]), distance_upper_bound=max_sep)
-        if (d != np.inf):
-            idxs.append(i)
 
-    sex_matched = sex[idxs]
+    sex_idxs = []
+    phot_idxs = []
+    for i in range(len(sex)):
+        d, ii = tree.query((sex['X_IMAGE'][i], sex['Y_IMAGE'][i]), distance_upper_bound=max_sep)
+        if (d != np.inf):
+            sex_idxs.append(i)
+            phot_idxs.append(ii)
+
+    sex_matched = sex[sex_idxs]
+    phot_matched = phot[phot_idxs]
 
     with open(newphotfile, 'w') as outfile:
         with open(photfile, 'r') as infile:
@@ -253,20 +200,64 @@ def write_phot(photfile, newphotfile, sexfile):
                 if line.startswith('#'):
                     outfile.write(line)
 
-        for i in range(len(phot)):
-            if (phot['MAG'][i] > 0.) and (sex_matched['MAG_APER'][i] < 99.):
+        for i in range(len(sex_idxs)):
+            if (phot_matched['MAG'][i] > 0.) and (sex['MAG_APER'][i] < 99.):
                 outfile.write("{:<23}{:<10.3f}{:<10.3f}{:<6d}{:<23}{:<6d} \\\n".format(
-                    phot['IMAGE'][i], phot['XINIT'][i], phot['YINIT'][i], phot['ID'][i], phot['COORDS'][i],
-                    phot['LID'][i]))
+                    phot_matched['IMAGE'][i], phot_matched['XINIT'][i], phot_matched['YINIT'][i], phot_matched['ID'][i],
+                    phot_matched['COORDS'][i], phot_matched['LID'][i]))
                 outfile.write("   {:<11.3f}{:<11.3f}0.000   0.000   INDEF   INDEF          0    NoError   \\\n".format(
-                    phot['XCENTER'][i], phot['YCENTER'][i]))
+                    phot_matched['XCENTER'][i], phot_matched['YCENTER'][i]))
                 outfile.write("   {:<15.7g}{:<15.7g}{:<15.7g}{:<7d}{:<9d}0    NoError   \\\n".format(
-                    phot['MSKY'][i], phot['STDEV'][i], phot['SSKEW'][i], phot['NSKY'][i], phot['NSREJ'][i]))
+                    phot_matched['MSKY'][i], phot_matched['STDEV'][i], phot_matched['SSKEW'][i],
+                    phot_matched['NSKY'][i], phot_matched['NSREJ'][i]))
                 outfile.write("   1.             INDEF          INDEF                  INDEF                  \\\n")
                 outfile.write("   8.00     {:<14.7g}{:<11.7g}{:<14.7g}{:<7.3f}{:<6.3f}0    NoError    \n".format(
-                    phot['SUM'][i], phot['AREA'][i], sex_matched['FLUX_APER'][i], sex_matched['MAG_APER'][i],
-                    sex_matched['MAGERR_APER'][i]))
+                    phot_matched['SUM'][i], phot_matched['AREA'][i], sex_matched['FLUX_APER'][i],
+                    sex_matched['MAG_APER'][i], sex_matched['MAGERR_APER'][i]))
 
+    return
+
+
+def remove_saturated(image, sexfile, coofile, newcoofile, max_zeros=1.):
+    sex_data = ascii.read(sexfile, format='sextractor')
+    coo_data = ascii.read(coofile, format='daophot')
+
+    with fits.open(image) as hdu:
+        img_data = hdu[0].data
+
+    x = sex_data['X_IMAGE']
+    y = sex_data['Y_IMAGE']
+    r = sex_data['KRON_RADIUS']
+
+    idx = []
+    for i in range(len(x)):
+        zeros = []
+        for xx in range(int(x[i]) - int(r[i]), int(x[i]) + int(r[i])):
+            yy_p = np.sqrt(r[i] ** 2 - (xx - int(x[i])) ** 2) + int(y[i])
+            yy_m = -np.sqrt(r[i] ** 2 - (xx - int(x[i])) ** 2) + int(y[i])
+            for yy in range(int(yy_m), int(yy_p)):
+                if img_data[yy, xx] == 0.:
+                    zeros.append(1.)
+        if len(zeros) <= max_zeros:
+            idx.append(i)
+
+    with open(newcoofile, 'w') as outfile:
+        with open(coofile, 'r') as infile:
+            for line in infile:
+                if line.startswith('#'):
+                    outfile.write(line)
+        for i in range(len(idx)):
+            # N XCENTER   YCENTER   MAG      SHARPNESS   SROUND      GROUND      ID         \
+            # U pixels    pixels    #        #           #           #           #          \
+            # F %-13.3f   %-10.3f   %-9.3f   %-12.3f     %-12.3f     %-12.3f     %-6d       \
+            outfile.write("{:<13.3f}{:<10.3f}{:<9.3f}{:<12.3f}{:<12.3f}{:<12.3f}{:<6d}\n".format(coo_data['XCENTER'][i],
+                                                                                                 coo_data['YCENTER'][i],
+                                                                                                 coo_data['MAG'][i],
+                                                                                                 coo_data['SHARPNESS'][
+                                                                                                     i],
+                                                                                                 coo_data['SROUND'][i],
+                                                                                                 coo_data['GROUND'][i],
+                                                                                                 coo_data['ID'][i]))
     return
 
 
