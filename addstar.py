21fa6733413d78cf9f115afd39d52261880c7a29
kawebb <kristiannewebb@gmail.com>
2016-03-06 18:30:36 -0800

    rewritten once again, less output files

--- addstar.py
+++ addstar.py
@@ -12,62 +12,68 @@ from matplotlib.colors import LogNorm
 """
 Create artificial stars from a good candidate star in the image. Determine number of stars recovered.
 
-python addstar.py -sf phot_t3/CB68_J_sex_t3_ap30.txt -img CB68/CB68_J_sub.fits -artimg mag_lim/CB68_J_sub_21p0074.art{}.fits -mags 21.006 21.1007
-python addstar.py -sf phot_t3/CB68_J_sex_t3_ap30.txt -img release/CB68_J_sub.fits -artimg mag_lim/CB68_J_sub_temp.art{}.fits -mags 23.1 24.0
-
-sex -c phot_t3.sex ../mag_lim/CB68_J_sub_21p0074.art1.fits -CATALOG_NAME ../mag_lim/CB68_J_sex_21p0074.art1.txt -PHOT_APERTURES 30 -MAG_ZEROPOINT 29.80705
-sex -c phot_t3.sex ../mag_lim/CB68_J_sub_21p0074.art2.fits -CATALOG_NAME ../mag_lim/CB68_J_sex_21p0074.art2.txt -PHOT_APERTURES 30 -MAG_ZEROPOINT 29.80705
-sex -c phot_t3.sex ../mag_lim/CB68_J_sub_21p0074.art3.fits -CATALOG_NAME ../mag_lim/CB68_J_sex_21p0074.art3.txt -PHOT_APERTURES 30 -MAG_ZEROPOINT 29.80705
-sex -c phot_t3.sex ../mag_lim/CB68_J_sub_21p0074.art4.fits -CATALOG_NAME ../mag_lim/CB68_J_sex_21p0074.art4.txt -PHOT_APERTURES 30 -MAG_ZEROPOINT 29.80705
-sex -c phot_t3.sex ../mag_lim/CB68_J_sub_21p0074.art5.fits -CATALOG_NAME ../mag_lim/CB68_J_sex_21p0074.art5.txt -PHOT_APERTURES 30 -MAG_ZEROPOINT 29.80705
-
-python addstar.py -artsf mag_lim/CB68_J_sex_21p0074.art{}.txt --mag 21.0074
+Run addstars to generate model PSF and implant # of them into the image
+Run source extractor with code output to measure photometry on image with artificial stars
+Run addstars to counts number of artificial stars recovered
 """
 
 _SEXFILE = 'phot_t3/CB68_J_sex_t3_ap30.txt'
-_IMAGE = 'CB68/CB68_J_sub.fits'
-_ARTIMAGE = 'mag_lim/CB68_J_sub_temp.art{}.fits'
+# _IMAGE = 'CB68/CB68_J_sub.fits'
+_IMAGE = 'release/CB68_J_sub.fits'
+_ARTIMAGE = 'mag_lim/CB68_J_sub_{}_art.fits'
+
+###### Default values
+_MAGRANGE = [18.,24.]
 _STAR_INDICES = [None, None, None, None, None, None, None, None, None, None, None, None]
-_MAGS = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-_MAXMAGERR = 0.06
+_MAGS = [None, None, None, None, None, None, None, None, None, None, None, None]
+_MAXMAGERR = 0.01
 
-# _MAGRANGE = [21.0, 21.5]
-# _STAR_INDICES = [2,3,4,7,5,7,7,1,2, 2,10,12]
-# _MAGS = [21.1269,21.0681,21.1589,21.1441,21.0893,21.2558,21.0657,21.1308,21.1766,21.0787,21.2691,21.1766]
-# _MAXMAGERR = 0.03
+_ARTSEXFILE = 'mag_lim/CB68_J_sex_{}_art.txt'
+_ARTPNG = 'mag_lim/CB68_J_sex_{}_art.png'
 
-# _MAGRANGE = [21.5, 22.]
-# _STAR_INDICES = [4, 2, 3, 4, 3, 7, 8, 3, 2, 5, 2, 1]
-# _MAGS = [21.7131, 21.6848, 21.5841, 21.5804, 21.5654, 21.5035, 21.6421, 21.6441, 21.7821, 21.5449, 21.7918, 21.6072]
-# _MAXMAGERR = 0.05
-
-# _MAGRANGE = [22., 22.5]
-# _STAR_INDICES = [0,0,0,2,1,0,1,1,0,1,1,0]
-# _MAGS = [22.0183, 22.067, 22.0456, 22.0486, 22.0165, 22.0436, 22.0496, 22.0325, 22.1663, 22.0496, 22.2055, 22.0092]
-# _MAXMAGERR = 0.06
-
-# _MAGRANGE = [22.5, 23.]
-# _STAR_INDICES = [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0]
-# _MAGS = [22.6074, 22.5360, 22.6229, 22.5411, 22.5966, 22.6141, 22.56, 22.5042, 22.5526, 22.562, 22.5042, 22.969]
-# MAXMAGERR = 0.06
-
-_MAGRANGE = [23., 25.]
-# art5 looks waaaay to high
-_STAR_INDICES = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-_MAGS = [23.1053, 23.1185, 23.0592, 23.1472, 23.5124, 23.03, 23.0679, 23.016, 23.0441, 23.2377, 23.016, 23.3859]
-_MAXMAGERR = 0.06
-
-# 10 seems to vary a lot
 _ARTFILES = ['mag_lim/art1.coo', 'mag_lim/art2.coo', 'mag_lim/art3.coo', 'mag_lim/art4.coo', 'mag_lim/art5.coo',
-             'mag_lim/art6.coo', 'mag_lim/art7.coo', 'mag_lim/art8.coo', 'mag_lim/art9.coo', 'mag_lim/art10.coo',
-             'mag_lim/art11.coo', 'mag_lim/art12.coo']
-
+            'mag_lim/art6.coo', 'mag_lim/art7.coo', 'mag_lim/art8.coo', 'mag_lim/art9.coo', 'mag_lim/art10.coo',
+            'mag_lim/art11.coo', 'mag_lim/art12.coo']
 #               1                2              3               4              5                6                 7
 #               8                9              10              11             12
 _XRANGE = [[700., 2000.], [2100., 3400.], [3600., 4800.], [700., 2000.], [2100., 3400.], [3600., 4800.], [700., 2000.],
            [2100., 3400.], [3600., 4800.], [700., 2000.], [2100., 3400.], [3600., 4800.]]
 _YRANGE = [[3800., 4600.], [3800., 4600.], [3800., 4600.], [2800., 3600.], [2800., 3600.], [2800., 3600.],
            [1300., 2500.], [1300., 2500.], [1300., 2500.], [900., 1600.], [900., 1600.], [900., 1600.]]
+
+# _ARTSEXFILE = _SEXFILE
+# _ARTIMAGE = _IMAGE
+# _MAGS = [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
+
+# CB68 J
+#
+#_MAGRANGE = [21.0, 21.5]
+#_STAR_INDICES = [2,3,4,7,5,7,7,1,2, 2,10,12]
+#_MAGS = [21.1269,21.0681,21.1589,21.1441,21.0893,21.2558,21.0657,21.1308,21.1766,21.0787,21.2691,21.1766]
+#_MAXMAGERR = 0.03
+
+#_MAGRANGE = [21.5, 22.]
+#_STAR_INDICES = [4, 2, 3, 4, 3, 7, 8, 3, 2, 5, 2, 1]
+#_MAGS = [21.7131, 21.6848, 21.5841, 21.5804, 21.5654, 21.5035, 21.6421, 21.6441, 21.7821, 21.5449, 21.7918, 21.6072]
+#_MAXMAGERR = 0.05
+
+#_MAGRANGE = [22., 22.5]
+#_STAR_INDICES = [0,0,0,2,1,0,1,1,0,1,1,0]
+#_MAGS = [22.0183, 22.067, 22.0456, 22.0486, 22.0165, 22.0436, 22.0496, 22.0325, 22.1663, 22.0496, 22.2055, 22.0092]
+#_MAXMAGERR = 0.06
+
+#_MAGRANGE = [22.5, 23.]
+#_STAR_INDICES = [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0]
+#_MAGS = [22.6074, 22.5360, 22.6229, 22.5411, 22.5966, 22.6141, 22.56, 22.5042, 22.5526, 22.562, 22.5042, 22.969]
+#_MAXMAGERR = 0.06
+
+#_MAGRANGE = [23., 25.]
+#_STAR_INDICES = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#_MAGS = [23.1053, 23.1185, 23.0592, 23.1472, 23.5124, 23.03, 23.0679, 23.016, 23.0441, 23.2377, 23.016, 23.3859]
+#_MAXMAGERR = 0.06
+
+
+# More default values
 _DIMENSION = 25.
 _NSTARS = 100.
 _MAXCOUNTS = np.inf
@@ -76,11 +82,6 @@ _MIN_MAXCOUNTS = -np.inf
 _TOPLOT = False
 _CHI2MAX = 100.
 _FUNCTION = 'gauss'
-
-# _ARTSEXFILE = None
-_ARTSEXFILE = 'mag_lim/CB68_J_sex_{}.art{}.txt'
-# _ARTSEXFILE = _SEXFILE
-_ARTPNG = 'mag_lim/CB68_J_sex_{}_art.png'
 
 
 def main():
@@ -96,15 +97,16 @@ def main():
     #          [30 + 0.605746118244, 30 + 1.07900318805, 30 + 0.859804630004, 30 + 0.208893637398],
     #          [30 + 0.466909777698, 30 + 0.776624355589, 30 + 0.552873836659, 30 - 1.14661744067]]
 
+    for i, artfile in enumerate(_ARTFILES):
+        if not os.path.exists(artfile):
+            generate_coords(artfile, int(_NSTARS), _XRANGE[i], _YRANGE[i])
 
-    if _ARTSEXFILE is None:
+    magstr = '{:<4.4f}'.format(np.mean(_MAGS)).replace('.', 'p')
+    artsexfile = _ARTSEXFILE.format(magstr)
+
+    if not os.path.exists(artsexfile):
         if _ARTIMAGE is None:
             raise Exception, 'No output image file format is given'
-        artimages = []
-        for i, artfile in enumerate(_ARTFILES):
-            if not os.path.exists(artfile):
-                generate_coords(artfile, int(_NSTARS), _XRANGE[i], _YRANGE[i])
-            artimages.append(_ARTIMAGE.format(i + 1))
         if _SEXFILE is None:
             raise Exception, 'No input source extractor file given'
         if _IMAGE is None:
@@ -116,51 +118,61 @@ def main():
 
         indices = []
         magnitudes = []
+        x_art = np.array([])
+        y_art = np.array([])
 
         print '*********************************************************************'
         print '**                           ADDSTAR                               **'
         print '*********************************************************************'
 
+        with fits.open(_IMAGE) as hdu:
+            imgdata = hdu[0].data
+
         for i in range(len(_XRANGE)):
-            idx, mag = addstar(_SEXFILE, _IMAGE, _ARTFILES[i], artimages[i], _MAGRANGE, _STAR_INDICES[i], _XRANGE[i],
+            art_imgdata, x, y, idx, mag = addstar(_SEXFILE, _IMAGE, _ARTFILES[i], _MAGRANGE, _STAR_INDICES[i], _XRANGE[i],
                                _YRANGE[i], float(_MAXMAGERR), float(_CHI2MAX), float(_MINCOUNTS), float(_MAXCOUNTS),
                                float(_MIN_MAXCOUNTS), _DIMENSION, _FUNCTION, _TOPLOT)
             indices.append(idx)
             magnitudes.append(mag)
+            imgdata += art_imgdata
+            x_art = np.concatenate((x_art, x))
+            y_art = np.concatenate((y_art, y))
+
+        if _TOPLOT:
+            plt.imshow(imgdata, origin='bottom', norm=LogNorm())
+            plt.scatter(x_art, y_art, marker='x', color='k')
+            plt.show()
+
+        magstr = '{:<4.4f}'.format(np.mean(magnitudes)).replace('.', 'p')
+        outimage = _ARTIMAGE.format(magstr).replace('temp', magstr)
+        print '  Writing image with artificial stars: {}'.format(outimage)
+        hdu = fits.PrimaryHDU(1)
+        hdu.data = imgdata
+        hdu.writeto(outimage, clobber=True)
 
         print indices
         print magnitudes
 
-        # with open('phot_t3/runartsex.sh', 'a') as outfile:
-        #     for i in range(len(_XRANGE)):
-        #         magstr = str(_MAGS[i]).replace('.', 'p')
-        #         outfile.write('sex -c phot_t3.sex ../mag_lim/CB68_J_sub_{}.art{}.fits -CATALOG_NAME ../mag_lim/CB68_J_sex_{}.art{}.txt -PHOT_APERTURES 30 -MAG_ZEROPOINT 29.80705 \n'.format(magstr, i+1, magstr, i+1))
+        outartsexfile = _ARTSEXFILE.format(magstr)
+        print 'sex -c phot_t3.sex ../{} -CATALOG_NAME ../{} -PHOT_APERTURES 30 -MAG_ZEROPOINT 29.80705 \n'.format(outimage, outartsexfile)
 
-    if _ARTSEXFILE is not None:
 
-        artpngs = []
-        artsexfiles = []
-        for i, mag in enumerate(_MAGS):
-            magstr = str(mag).replace('.', 'p')
-            artsexfiles.append(_ARTSEXFILE.format(magstr, i + 1))
-            artpngs.append(_ARTPNG.format(magstr, i + 1))
-
-        magstr = '{:<2.4f}'.format(np.mean(_MAGS)).replace('.', 'p')
+    if os.path.exists(artsexfile):
         fig_regions = _ARTPNG.format(magstr + '_each')
         fig_all = _ARTPNG.format(magstr + '_all')
-        find_art_stars_allregions(artsexfiles, _ARTFILES, _MAGS, _TOPLOT, fig_regions, fig_all)
+        find_art_stars_allregions(artsexfile, _ARTFILES, _MAGS, _TOPLOT, fig_regions, fig_all)
 
 
-def find_art_stars_allregions(artsexfiles, artfiles, mags, toplot, fig_regions, fig_all):
+def find_art_stars_allregions(artsexfile, artfiles, mags, toplot, fig_regions, fig_all):
     allartmags = np.array([])
     recovered = []
     artmagdiff = []
 
     fig = plt.figure(figsize=(10, 10))
-    for i, artsexfile in enumerate(artsexfiles):
+    for i, artfile in enumerate(artfiles):
         ax = fig.add_subplot(4, 3, i + 1)
 
-        artmags, recov, magdiff = find_art_stars(artsexfile, artfiles[i], mags[i], toplot=toplot, writeto=toplot)
+        artmags, recov, magdiff = find_art_stars(artsexfile, artfile, mags[i], toplot=toplot, writeto=toplot)
 
         if len(artmags) > 1:
             n, bins, patches = plt.hist(artmags, 50, color='b', alpha=0.5, label='{:<2.1f}%'.format(recov * 100.))
@@ -224,7 +236,7 @@ def find_art_stars(artsexfile, artfile, mag=None, max_sep=2., toplot=False, writ
 
     if toplot or writeto:
 
-        n, bins, patches = plt.hist(mags, 50, color='b', alpha=0.5, label=recovered)
+        n, bins, patches = plt.hist(mags, 50, color='b', alpha=0.5, label='{}/{}'.format(len(idx_found), len(x_pix)*100))
         plt.axvline(mags_mean, color='k', label='mean {:<4.3}'.format(mags_mean))
         plt.axvline(mag, 0, 10, color='r', label='PSF mag {}'.format(mag))
         plt.legend(loc=0)
@@ -241,7 +253,7 @@ def find_art_stars(artsexfile, artfile, mag=None, max_sep=2., toplot=False, writ
     return mags, n_found / n_art, float(mag) - mags_mean
 
 
-def addstar(sexfile, image, artfile, artimage, (magmin, magmax), star_idx=None, (xmin, xmax)=(600., 4800.),
+def addstar(sexfile, image, artfile, (magmin, magmax), star_idx=None, (xmin, xmax)=(600., 4800.),
             (ymin, ymax)=(600., 4800.), mag_limit=0.01, chi2max=100., mincounts=-np.inf, maxcounts=np.inf,
             min_maxcounts=-np.inf, s=25., function='gauss', toplot=False):
     """
@@ -281,8 +293,9 @@ def addstar(sexfile, image, artfile, artimage, (magmin, magmax), star_idx=None, 
     print '>>  {} Stars found'.format(np.shape(stars)[0])
 
     # If a particular star is not specified in the command line, plot potential stars until one chosen
-    print '  Finding star PSFs to model: type a number to select the corresponding indexed star, "enter" to continue, any string to kill'
     if star_idx is None:
+        print stars
+        print '  Finding star PSFs to model: type a number to select the corresponding indexed star, "enter" to continue, any string to kill'
         star_idx = False
         i = 0
         counter = 0  # not used for anything except having something in the exception line
@@ -294,9 +307,7 @@ def addstar(sexfile, image, artfile, artimage, (magmin, magmax), star_idx=None, 
             if (np.min(cutout) > mincounts) & (np.max(cutout) < maxcounts) & (np.max(cutout) > min_maxcounts):
 
                 try:  # use a try/except statements to avoid errors if fit fails, just skip those unable to be fit
-                    # fit the data in the cutout with a 2d gaussian
                     xx, yy = np.meshgrid(np.arange(s), np.arange(s))
-
                     if function == 'gauss':
                         popt, pcov = curve_fit(gaussian_2d, (xx, yy), cutout.ravel(),
                                                p0=[np.max(cutout), s / 2., s / 2., s / 2., s / 2., 0., 0.])
@@ -312,8 +323,8 @@ def addstar(sexfile, image, artfile, artimage, (magmin, magmax), star_idx=None, 
 
                     # only consider those with chi squares less than user specifed value
                     if np.abs(chi2 / dof) < chi2max:
-                        print '{} {} {} {} {}'.format(i, stars['X_IMAGE'][i], stars['Y_IMAGE'][i], stars['MAG_APER'][i],
-                                                      np.abs(chi2 / dof), np.max(cutout))
+                        print '   {} {} {} {} {}'.format(i, stars['X_IMAGE'][i], stars['Y_IMAGE'][i],
+                                                         stars['MAG_APER'][i], np.abs(chi2 / dof), np.max(cutout))
 
                         fig = plt.figure()
                         ax = fig.add_subplot(111, projection='3d')
@@ -337,8 +348,8 @@ def addstar(sexfile, image, artfile, artimage, (magmin, magmax), star_idx=None, 
 
     print '*******'
     print '  Star selected for PSF modeling:'
-    print '     idx     x [pix]   y [pix]   mag'
-    print '  {} {} {} {}'.format(star_idx, star['X_IMAGE'], star['Y_IMAGE'], star['MAG_APER'])
+    print '  idx  x [pix]   y [pix]   mag'
+    print '  {}   {} {} {}'.format(star_idx, star['X_IMAGE'], star['Y_IMAGE'], star['MAG_APER'])
 
     # cutout a region around the chosen star
     x, y = star['X_IMAGE'], star['Y_IMAGE']
@@ -351,13 +362,13 @@ def addstar(sexfile, image, artfile, artimage, (magmin, magmax), star_idx=None, 
                                p0=[np.max(cutout), s / 2., s / 2., s / 2., s / 2., 0., 0.])
         data_fitted = gaussian_2d((xx, yy), *popt).reshape(s, s)
         # create a 2D gaussian with (optimized) parameters returned from the fit, but centered
-        star_centered = gaussian_2d((xx, yy), popt[0], s / 2., s / 2., popt[3], popt[4], popt[5], 0.).reshape(s, s)
+        star_centered = gaussian_2d((xx, yy), popt[0], s / 2., s / 2., popt[3], popt[4], popt[5], popt[6]).reshape(s, s)
 
     if function == 'lorentz':
         popt, pcov = curve_fit(lorentz_2d, (xx, yy), cutout.ravel(),
                                p0=[np.max(cutout), s / 2., s / 2., s / 50., s / 50., 0., 0.])
         data_fitted = lorentz_2d((xx, yy), *popt).reshape(s, s)
-        star_centered = lorentz_2d((xx, yy), popt[0], s / 2., s / 2., popt[3], popt[4], popt[5], 0.).reshape(s, s)
+        star_centered = lorentz_2d((xx, yy), popt[0], s / 2., s / 2., popt[3], popt[4], popt[5], popt[6]).reshape(s, s)
 
     print '  Optimal parameters, A, x0, y0, sigma_x, sigma_y, theta, offset:  '
     print '    {} {} {} {} {} {} {}'.format(popt[0], popt[1], popt[2], popt[3], popt[4], popt[5], popt[6])
@@ -382,23 +393,11 @@ def addstar(sexfile, image, artfile, artimage, (magmin, magmax), star_idx=None, 
 
     # implant the gaussian "artificial star" at each location
     x_art, y_art = np.loadtxt(artfile, unpack=True, delimiter=' ')
-    art_imgdata = imgdata
+    art_imgdata = imgdata*0.
     for n in range(len(x_art)):
         art_imgdata[y_art[n] - s / 2.:y_art[n] + s / 2., x_art[n] - s / 2.:x_art[n] + s / 2.] += star_centered
 
-    if toplot:
-        plt.imshow(art_imgdata, origin='bottom', norm=LogNorm())
-        plt.scatter(x_art, y_art, marker='x', color='k')
-        plt.show()
-
-    magstr = str(star['MAG_APER']).replace('.', 'p')
-    outimage = artimage.replace('temp', magstr)
-    print '  Writing image with artificial stars: {}'.format(outimage)
-    hdu = fits.PrimaryHDU(1)
-    hdu.data = art_imgdata
-    hdu.writeto(outimage, clobber=True)
-
-    return int(star_idx), star['MAG_APER']
+    return art_imgdata, x_art, y_art, int(star_idx), star['MAG_APER']
 
 
 def generate_coords(artfile, nstars=100., (xmin, xmax)=(600., 4800.), (ymin, ymax)=(600., 4800.)):
